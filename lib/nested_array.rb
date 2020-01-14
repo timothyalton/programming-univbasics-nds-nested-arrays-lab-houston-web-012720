0dc0177777ce66922d1b73cdfe6843dea8dab2e7
Mitchell T Alton <mitchell.alton@live.com>
2020-01-14 22:43:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,6 +40,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  matrix_lookup[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
